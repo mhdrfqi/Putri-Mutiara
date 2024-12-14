c5e4ec83b11c3b59f6949c327abfeddac1082823
mhdrfqi <160157536+mhdrfqi@users.noreply.github.com>
2024-12-14 13:08:45 +0800

    Add files via upload

---
+++ web apalo/web.js
@@ -0,0 +1,3 @@
+document.getElementById('forgiveButton').addEventListener('click', function () {
+    alert('Terima kasih telah memaafkanku, Putri Mutiara. Aku akan berusaha menjadi lebih baik!');
+});
